614dc748762f78cd86944efb1db8d24614246f1e
Musyoka552 <kilonzopatoh6@gmail.com>
2024-06-27 22:31:55 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+let multiply = 62;
+let num1 =multiply/num2;
+let num2 =multiply/num1;
+let random = Math.floor(Math.random()* 10)+ 1;
+let num3;
+let num4;
+let mod =num3%num4;
+let max = Math.max(10,20);
 // Write your code here
